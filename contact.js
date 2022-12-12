eab6f477f0f31cc045241fcc89bc0af3a02f5da7
rohan <syedruhan8@gmail.com>
2022-12-12 21:22:10 +0600

    update

---
+++ contact.js
@@ -0,0 +1,58 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCV-KGEqIx-xJR5f0qvr4TP5MAHcuFNgaw",
+  authDomain: "contact-form-c4c51.firebaseapp.com",
+  databaseURL: "https://contact-form-c4c51-default-rtdb.firebaseio.com",
+  projectId: "contact-form-c4c51",
+  storageBucket: "contact-form-c4c51.appspot.com",
+  messagingSenderId: "148768869109",
+  appId: "1:148768869109:web:1cfa92ad3e0415969a93bc",
+  measurementId: "G-12TEFBP5Q1"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById("contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var name = getElementVal("name");
+  var emailid = getElementVal("emailid");
+  var phone = getElementVal("phone");
+  var address = getElementVal("address");
+  var msgContent = getElementVal("msgContent");
+  
+
+  saveMessages(name, emailid,phone, address, msgContent);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("contactForm").reset();
+}
+
+const saveMessages = (name, emailid,phone,address, msgContent) => {
+  var newContactForm = contactFormDB.push();
+
+  newContactForm.set({
+    name: name,
+    emailid: emailid,
+    phone:phone,
+    address:address,
+    msgContent: msgContent,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
